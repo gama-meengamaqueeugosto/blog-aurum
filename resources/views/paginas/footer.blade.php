99f462371bf57b49aa5a46db0ee3b337ad98b732
Alexandro Feijó <desenvolvimento@cenazero.com>
2017-09-20 10:21:45 -0300

    alteracao rodapé

--- resources/views/paginas/footer.blade.php
+++ resources/views/paginas/footer.blade.php
@@ -6,6 +6,12 @@
                     <h2>Advogado Moderno</h2>
                     <p>Copyright © 2017.</p>
                 </div>
+                <div class="col-md-12">
+                    <small>
+                        <span>Imagens desenvolvidas por </span>
+                        <p><a href="http://www.freepik.com" target="_blank">Freepik.com - Recursos gráficos gratuitos para designers</a></p>
+                    </small>
+                </div>
             </div>
         </div>
     </div>
